fe91c02e65ee000182356fe66e0917019ec5b668
mnosel00 <mateusz.nosel@microsoft.wsei.edu.pl>
2022-03-09 13:55:42 +0100

    kalkulator

--- Lab1-09-03/index.js
+++ Lab1-09-03/index.js
@@ -1,6 +1,32 @@
+const { query } = require('express');
 var express = require('express');
 var app = express();
-app.get('/', function (req, res) {
-    res.send('Hello world');
-});
+app.get('/add', function(req, res){
+    var a = Number(req.query.num1);
+    var b = Number(req.query.num2);
+    
+    var resuslt = a+b;
+    res.send('wynik dodawania: ' + resuslt);
+  });
+  app.get('/odejmowanie', function(req, res){
+    var a = Number(req.query.num1);
+    var b = Number(req.query.num2);
+    
+    var resuslt = a-b;
+    res.send('wynik dodawania: ' + resuslt);
+  });
+  app.get('/mnozenie', function(req, res){
+    var a = Number(req.query.num1);
+    var b = Number(req.query.num2);
+    
+    var resuslt = a*b;
+    res.send('wynik dodawania: ' + resuslt);
+  });
+  app.get('/dzielenie', function(req, res){
+    var a = Number(req.query.num1);
+    var b = Number(req.query.num2);
+    
+    var resuslt = a/b;
+    res.send('wynik dodawania: ' + resuslt);
+  });
 app.listen(3000);
--- Lab1-09-03/index.ts
+++ Lab1-09-03/index.ts
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-app.get('/',function(req,res){
+app.get('/id',function(req,res){
     res.send('Hello world');
+    
 })
 app.listen(3000);
